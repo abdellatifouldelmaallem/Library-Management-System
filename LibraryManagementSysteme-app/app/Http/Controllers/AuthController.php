b127b4ab51f270a8da73a4b7b4fe2268b7c433f2
abdellatif <ouldeabdellatif40@gmail.com>
2021-08-03 00:15:35 +0100

    add login and logout

--- LibraryManagementSysteme-app/app/Http/Controllers/AuthController.php
+++ LibraryManagementSysteme-app/app/Http/Controllers/AuthController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Librarian;
 use Illuminate\Http\Request;
 use App\Models\reader;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    // register for reader
     public function registerReader(Request $request){
     
         $info = $request->validate([
@@ -32,4 +35,102 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+
+    // logout for reader
+
+    public function logoutForReader(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        
+        return response('message:log out');
+    }
+
+    
+    //login for reader
+    public function loginReader(Request $request){
+
+        $info = $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string',
+           ]);
+   
+          // check the email
+          $reader = reader::where('email',$request['email'])->first();
+
+          //check the password
+          if(!$reader || !Hash::check($request['password'], $reader->password)){
+             return response([
+                'message'=>'your data can not let to access'
+             ], 401);
+          }
+           
+            $token= $reader->createToken('readerToken')->plainTextToken;
+           
+           $response = [
+               'reader'=> $reader,
+               'token'=>$token, 
+           ];
+   
+           return response($response, 201);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    // register for librarian
+    public function registerLibrarian(Request $request){
+
+        $info = $request->validate([
+            'fullName'=>'required|string',
+            'nameOfLibrary'=>'required|string',
+            'email'=>'required|string|unique:reader',
+            'password'=>'required|string|confirmed',
+        ]);
+
+        $librarian = Librarian::create([
+            'fullName'=>$info['fullName'],
+            'nameOfLibrary'=>$info['nameOfLibrary'],
+            'email'=>$info['email'],
+            'password'=>bcrypt($info['password']),
+        ]);
+
+        $token = $librarian->createToken('librarianToken')->plainTextToken;
+
+        $response = [
+            'librarian'=> $librarian,
+            'token'=>$token, 
+        ];
+
+        return response($response, 201);
+    }
+
+    // login for librarian
+    public function loginLibrarian(Request $request){
+
+      
+        $info = $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string',
+           ]);
+   
+          // check the email
+          $librarian = Librarian::where('email',$request['email'])->first();
+
+          //check the password
+          if(!$librarian || !Hash::check($request['password'], $librarian->password)){
+             return response([
+                'message'=>'your data can not let to access'
+             ], 401);
+          }
+           
+            $token= $librarian->createToken('librarianToken')->plainTextToken;
+           
+           $response = [
+               'librarian'=> $librarian,
+               'token'=>$token, 
+           ];
+   
+           return response($response, 201);
+    }
+
+
+
 }
